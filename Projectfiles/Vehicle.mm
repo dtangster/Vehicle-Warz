fab3ba059535395a21f88472bc46784e05ea6b3c
Akshay Hegde <akshay.hegde17@gmail.com>
2013-06-26 12:52:49 -0700

    Revert Vehicle.mm to before synthesizing

--- Projectfiles/Vehicle.mm
+++ Projectfiles/Vehicle.mm
@@ -10,43 +10,20 @@
 
 @implementation Vehicle
 
-@synthesize vehicleName = _vehicleName;
-@synthesize health = _health;
-@synthesize shield = _shield;
-@synthesize power = _power;
-@synthesize speed = _speed;
-@synthesize energy = _energy;
-@synthesize lastAngle = _lastAngle;
-
 -(id) initWithName:(NSString *) vehicleName usingImage:(NSString *) fileName
 {
     if ((self = [super initWithFile:fileName]))
     {
-        vehicleName = _vehicleName;
-        _health = 2;
-        _shield = 2;
-        _power = 100;
-        _speed = 2;
-        _energy = 100;
-        _lastAngle = 45;
+        self.vehicleName = vehicleName;
+        self.health = 2;
+        self.shield = 2;
+        self.power = 100;
+        self.speed = 2;
+        self.energy = 100;
+        self.lastAngle = 45;
     }
+    
     return self;
 }
 
-- (void)setPower:(int)power
-{
-    _power = power;
-}
-
-- (int)power
-{
-    if (_power > 100) {
-        [self setPower:100];
-    }
-    else if (_power < 0) {
-        [self setPower:0];
-    }
-    
-    return _power;
-}
 @end
