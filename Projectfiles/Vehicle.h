24c0e751d4eab9d09ba8335ce76749567bad73f7
David Tang <Tang@Davids-MacBook-Air.local>
2013-06-27 13:31:23 -0700

    Added comments to Vehicle.h

--- Projectfiles/Vehicle.h
+++ Projectfiles/Vehicle.h
@@ -14,26 +14,40 @@
 @interface Vehicle : CCSprite
 
 @property (nonatomic) NSString *vehicleName;
-@property (nonatomic) b2Fixture *fixture; //will store the shape and density information
-@property (nonatomic) b2Body *body;  //will store the position and type
+@property (nonatomic) b2Fixture *fixture; // Will store the shape and density information
+@property (nonatomic) b2Body *body;  // Will store the position and type
 @property (nonatomic) Weapon *weapon1;
 @property (nonatomic) Weapon *weapon2;
 @property (nonatomic) Weapon *special;
+
+// These are the base stats that multiplier effects will be applied to
 @property (nonatomic) int baseHealth;
 @property (nonatomic) int baseShield;
 @property (nonatomic) int basePower;
 @property (nonatomic) int baseSpeed;
 @property (nonatomic) int baseEnergy;
+
+// These are the stats after applying level up attributes and items. (IE. 40/120 HP where 120 is the max)
 @property (nonatomic) int maxHealth;
 @property (nonatomic) int maxShield;
+@property (nonatomic) int maxPower;
+@property (nonatomic) int maxSpeed;
 @property (nonatomic) int maxEnergy;
+
+// These are the stats of the max version minus any damage done, buffs or debuffs applied
 @property (nonatomic) int health;
 @property (nonatomic) int shield;
 @property (nonatomic) int power;
 @property (nonatomic) int speed;
 @property (nonatomic) int energy;
+
+// The max angle above its horizontal line
 @property (nonatomic) int maxFrontUpperAngle;
+
+// The max angle below its horizontal line
 @property (nonatomic) int maxFrontLowerAngle;
+
+// Store last shot settings
 @property (nonatomic) int lastShotPower;
 @property (nonatomic) int lastAngle;
 
