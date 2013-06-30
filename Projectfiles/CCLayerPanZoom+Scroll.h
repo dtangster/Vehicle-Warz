f63542f41ae83a4ce3c32c2a9d7f6402eff321ce
Akshay Hegde <akshay.hegde17@gmail.com>
2013-06-30 00:48:43 -0700

    Readd the category files

---
+++ Projectfiles/CCLayerPanZoom+Scroll.h
@@ -0,0 +1,13 @@
+//
+//  CCLayerPanZoom+Scroll.h
+//  Template Penguin
+//
+//  Created by Akshay on 6/30/13.
+//
+//
+
+#import "CCLayerPanZoom.h"
+
+@interface CCLayerPanZoom (Scroll)
+
+@end
---
+++ Projectfiles/CCLayerPanZoom+Scroll.m
@@ -0,0 +1,13 @@
+//
+//  CCLayerPanZoom+Scroll.m
+//  Template Penguin
+//
+//  Created by Akshay on 6/30/13.
+//
+//
+
+#import "CCLayerPanZoom+Scroll.h"
+
+@implementation CCLayerPanZoom (Scroll)
+
+@end
