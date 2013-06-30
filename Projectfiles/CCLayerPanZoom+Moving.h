8e05e974bb2586185caf2cc1b3bb20d71f250655
Akshay Hegde <akshay.hegde17@gmail.com>
2013-06-30 00:23:21 -0700

    Add Moving category of CCLayerPanZoom

---
+++ Projectfiles/CCLayerPanZoom+Moving.h
@@ -0,0 +1,13 @@
+//
+//  CCLayerPanZoom+Moving.h
+//  Template Penguin
+//
+//  Created by Akshay on 6/30/13.
+//
+//
+
+#import "CCLayerPanZoom.h"
+
+@interface CCLayerPanZoom (Moving)
+
+@end
---
+++ Projectfiles/CCLayerPanZoom+Moving.m
@@ -0,0 +1,13 @@
+//
+//  CCLayerPanZoom+Moving.m
+//  Template Penguin
+//
+//  Created by Akshay on 6/30/13.
+//
+//
+
+#import "CCLayerPanZoom+Moving.h"
+
+@implementation CCLayerPanZoom (Moving)
+
+@end
