c0cdea21c55c39425a7eb4818cd77c6b87ed138f
Akshay Hegde <akshay.hegde17@gmail.com>
2013-06-29 19:53:33 -0700

    Add StartMenuLayer for testing

---
+++ Projectfiles/StartMenuLayer.h
@@ -0,0 +1,13 @@
+//
+//  StartMenuLayer.h
+//  Template Penguin
+//
+//  Created by Akshay on 6/29/13.
+//
+//
+
+#import "CCLayer.h"
+
+@interface StartMenuLayer : CCLayer
+
+@end
---
+++ Projectfiles/StartMenuLayer.m
@@ -0,0 +1,13 @@
+//
+//  StartMenuLayer.m
+//  Template Penguin
+//
+//  Created by Akshay on 6/29/13.
+//
+//
+
+#import "StartMenuLayer.h"
+
+@implementation StartMenuLayer
+
+@end
