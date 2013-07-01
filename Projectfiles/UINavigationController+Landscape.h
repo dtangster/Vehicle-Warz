a4cb6d86176e6beb4272d74b19eaa3d0ccdbd7ee
Akshay Hegde <akshay.hegde17@gmail.com>
2013-07-01 02:57:46 -0700

    Add categories of UINavigationController

---
+++ Projectfiles/UINavigationController+Landscape.h
@@ -0,0 +1,15 @@
+//
+//  UINavigationController+Landscape.h
+//  Template Penguin
+//
+//  Created by Akshay on 7/1/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UINavigationController (Landscape)
+
+- (NSUInteger)supportedInterfaceOrientations;
+
+@end
---
+++ Projectfiles/UINavigationController+Landscape.m
@@ -0,0 +1,13 @@
+//
+//  UINavigationController+Landscape.m
+//  Template Penguin
+//
+//  Created by Akshay on 7/1/13.
+//
+//
+
+#import "UINavigationController+Landscape.h"
+
+@implementation UINavigationController (Landscape)
+
+@end
